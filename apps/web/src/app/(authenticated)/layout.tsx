3ffab2dd088ff2225bc2770cf91268f3ea93fd01
AnuragBaruah47 <anuragbaruah2004@gmail.com>
2026-01-17 16:19:46 +0530

    loader added

--- apps/web/src/app/(authenticated)/layout.tsx
+++ apps/web/src/app/(authenticated)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthenticatedLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<div className="eve h-screen">
+		<div className="eve h-fit">
 			<AuthenticatedHeader />
 			{children}
 		</div>
