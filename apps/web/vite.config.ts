8ca07abd3e4703cce0203f4b29c84d4cc8d9d25d
Aryan Bhati <codiearyan07@gmail.com>
2026-01-17 07:47:56 +0530

    vercel deployment fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,13 +3,24 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 export default defineConfig({
-	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
-	server: {
-		port: 3001,
-	},
-	ssr: {
-		noExternal: ["@convex-dev/better-auth"],
-	},
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    //this nitro plugin is a workaround to deploy on vercel
+    nitroV2Plugin /* 
+	// nitro config goes here, e.g.
+	{ preset: 'node-server' }
+  */(),
+  ],
+  server: {
+    port: 3001,
+  },
+
+  ssr: {
+    noExternal: ["@convex-dev/better-auth"],
+  },
 });
